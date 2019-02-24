91f4132b2d595a23f3a48120882ee8babc00d47f
Einar W. Høst <einarwh@gmail.com>
2019-02-24 09:24:40 +0100

    fish templates

---
+++ artstuff/gentemplates.sh
@@ -0,0 +1,6 @@
+gs -sDEVICE=pdfwrite -o sql-3-template-fish-1.pdf sql-3-template-fish-1.ps
+gs -sDEVICE=pdfwrite -o sql-3-template-fish-2.pdf sql-3-template-fish-2.ps
+gs -sDEVICE=pdfwrite -o sql-3-template-fish-3.pdf sql-3-template-fish-3.ps
+gs -sDEVICE=pdfwrite -o sql-4-template-fish-1.pdf sql-4-template-fish-1.ps
+gs -sDEVICE=pdfwrite -o sql-4-template-fish-2.pdf sql-4-template-fish-2.ps
+gs -sDEVICE=pdfwrite -o sql-4-template-fish-3.pdf sql-4-template-fish-3.ps
