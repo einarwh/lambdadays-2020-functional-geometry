6726ef70fdfca7e7d0fa4d736940d8a39ffef033
Einar W. Høst <einarwh@gmail.com>
2019-03-05 10:59:44 +0100

    level 5 sjablong

---
+++ artstuff/wintemplates.bat
@@ -0,0 +1,9 @@
+gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-1.pdf sql-3-template-fish-1.ps
+gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-2.pdf sql-3-template-fish-2.ps
+gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-3.pdf sql-3-template-fish-3.ps
+gswin64 -sDEVICE=pdfwrite -o sql-4-template-fish-1.pdf sql-4-template-fish-1.ps
+gswin64 -sDEVICE=pdfwrite -o sql-4-template-fish-2.pdf sql-4-template-fish-2.ps
+gswin64 -sDEVICE=pdfwrite -o sql-4-template-fish-3.pdf sql-4-template-fish-3.ps
+gswin64 -sDEVICE=pdfwrite -o sql-5-template-fish-1.pdf sql-5-template-fish-1.ps
+gswin64 -sDEVICE=pdfwrite -o sql-5-template-fish-2.pdf sql-5-template-fish-2.ps
+gswin64 -sDEVICE=pdfwrite -o sql-5-template-fish-3.pdf sql-5-template-fish-3.ps
