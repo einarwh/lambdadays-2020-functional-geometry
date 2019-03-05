09cde2259ee70056d15b35706536f7cfce3ca007
Einar W. Høst <einarwh@gmail.com>
2019-03-05 14:03:04 +0100

    more templates

--- artstuff/wintemplates.bat
+++ artstuff/wintemplates.bat
@@ -1,3 +1,6 @@
+gswin64 -sDEVICE=pdfwrite -o sql-2-template-fish-1.pdf sql-2-template-fish-1.ps
+gswin64 -sDEVICE=pdfwrite -o sql-2-template-fish-2.pdf sql-2-template-fish-2.ps
+gswin64 -sDEVICE=pdfwrite -o sql-2-template-fish-3.pdf sql-2-template-fish-3.ps
 gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-1.pdf sql-3-template-fish-1.ps
 gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-2.pdf sql-3-template-fish-2.ps
 gswin64 -sDEVICE=pdfwrite -o sql-3-template-fish-3.pdf sql-3-template-fish-3.ps
